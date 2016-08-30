f9ad7b6d522c7469a31d2e5edba6121c1e8118ba
Martyn Chamberlin <martyn@perfectioncoding.com>
2016-08-30 17:25:51 -0500

    Address issues

--- app/components/Header.js
+++ app/components/Header.js
@@ -8,29 +8,35 @@ export default class Header extends Component {
   // goHome() is just a stub
   goTo(slug) {
     chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
-      chrome.tabs.update(tabs[0].id, { url: 'https://www.chess.com/' + slug});
+      chrome.tabs.update(tabs[0].id, { url: 'https://www.chess.com/' + slug });
     });
   }
 
   render() {
-    var loginHeader = (<div></div>);
-    if (this.props.user.loggedIn) {
-      loginHeader = (
-        <div>
-          {this.props.user.username}
-          <img src={'http:' + this.props.user.avatarUrl}/>
-        </div>
-      );
-    } else if (!this.props.user.loading) {
-      loginHeader = (
-        <button onClick={this.goTo.bind(null, 'login')}>Login</button>
-      );
+    var userInfo = (<div></div>);
+    if (!this.props.user.loading) {
+      if (this.props.user.loggedIn) {
+        userInfo = (
+          <div>
+            {this.props.user.username}
+            <img src={'http:' + this.props.user.avatarUrl}/>
+          </div>
+        );
+      } else if (this.props.user.onChessCom && !this.props.user.onV3) {
+        userInfo = (
+          <button onClick={this.goTo.bind(null, 'switch')}>New Chess.com</button>
+        );
+      } else {
+        userInfo = (
+          <button onClick={this.goTo.bind(null, 'login')}>Login</button>
+        );
+      }
     }
 
     return (
       <header className={style.header}>
         <img src="img/logo.svg" height="25" role="presentation"/>
-        { loginHeader }
+        { userInfo }
       </header>
     );
   }
