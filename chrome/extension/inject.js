37ab65b93873b3dc8799f126cc7ad4bf43d4c4b8
Martyn Chamberlin <martyn@perfectioncoding.com>
2016-11-01 11:34:08 -0500

    Slow down polling for notifications to 60 seconds (#37)
    * Slow down polling for notifications to 30 seconds
    
    * Make that 60

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -82,7 +82,7 @@ function getNotifications() {
       return total;
     });
     return sendNotification(total, getNotifications);
-  }, 1000);
+  }, 60000);
 }
 
 window.addEventListener('load', () => {
