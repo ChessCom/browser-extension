c88e25346025d43f691aa458e7acb4ef09915f12
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-03-19 17:17:53 +0800

    Small change for neater syntax

--- chrome/extension/options.js
+++ chrome/extension/options.js
@@ -6,8 +6,7 @@ import styles from './options.css';
 // Get platformInfo before rendering Root
 chrome.runtime.getPlatformInfo(platformInfo => {
   if (platformInfo.os === 'mac') {
-    const body = document.getElementsByTagName('body')[0];
-    body.classList.add(styles['is-mac-os']);
+    document.body.classList.add(styles['is-mac-os']);
   }
   ReactDOM.render(
     <Root os={platformInfo.os} />,
