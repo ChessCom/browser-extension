cbcc28cbaae34e2b0bd137b6e9142361f2c8d509
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-03-20 10:06:57 +0800

    Cleaned up code regarding the chessBrowserExtension boolean set on window.context

--- chrome/getCurrentUser.js
+++ chrome/getCurrentUser.js
@@ -1,4 +1,9 @@
 if (window.context) {
+  // Chess.com sets the logged in user data on window.context
+  // which we fetch from inside the actual website and send to
+  // the extension through postMessage
   window.postMessage(window.context.user, '*');
+  // This lets Chess.com know that this user is using the extension
+  // which can help with troubleshooting / data handling etc.
   window.context.chessBrowserExtension = true;
 }
